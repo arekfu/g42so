262086e73502dbd1274ec9b54874b123ed162800
Davide Mancusi <davide.mancusi@cea.fr>
2015-02-05 08:42:50 +0100

    add optimization and debug flags

--- detect_compiler.py
+++ detect_compiler.py
@@ -2,9 +2,9 @@ from distutils.spawn import find_executable
 
 compilers = ['g++', 'clang++', 'icc']
 flags_dictionary = {
-        'g++': ['-fPIC', '-shared'],
-        'clang++': ['-fPIC', '-shared'],
-        'icc': [],
+        'g++': ['-O2', '-g', '-fPIC', '-shared'],
+        'clang++': ['-O2', '-g', '-fPIC', '-shared'],
+        'icc': [], # to be defined
         }
 
 def compiler_and_flags():
